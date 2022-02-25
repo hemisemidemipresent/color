b190854d2b13981f2bbd64d1e9a6bb3592a377c2
hemisemidemipresent <hnngggrrrr@gmail.com>
2022-02-25 13:38:49 +0800

    mr foo

--- index.js
+++ index.js
@@ -3,41 +3,68 @@ let colorOut = document.getElementById('colorOut');
 
 function convert() {
     let value = hentai.value;
-    if (isNaN(value) || value < 0 || value > 999999) {
-        return alert('enter hentai code from 000000 - 999999');
-    }
-    value = value.toString();
-    while (value.length < 6) {
-        value = '0' + value;
-    }
-    console.log(value);
-    let r = parseInt(value.substring(0, 2));
-    let g = parseInt(value.substring(2, 4));
-    let b = parseInt(value.substring(4, 6));
+    if (value.includes('#')) {
+        let hexStr = value.substring(1);
+        let hex = parseInt(hexStr, 16);
+        console.log(hex);
+        if (isNaN(hex) || hex < 0 || hex > 16777215) {
+            return alert('enter hexadecimal color code from #000000 - #ffffff');
+        } else {
+            body.style.backgroundColor = value;
 
-    let red = Math.round((r / 99) * 255);
-    let green = Math.round((g / 99) * 255);
-    let blue = Math.round((b / 99) * 255);
+            let redHex = hexStr.substring(0, 2);
+            let greenHex = hexStr.substring(2, 4);
+            let blueHex = hexStr.substring(4, 6);
 
-    console.log(red, green, blue);
-    red16 = red.toString(16) + '';
-    green16 = green.toString(16) + '';
-    blue16 = blue.toString(16) + '';
-    if (red16.length == 1) red16 = '0' + red16;
-    if (green16.length == 1) green16 = '0' + green16;
-    if (blue16.length == 1) blue16 = '0' + blue16;
-    console.log(red16, green16, blue16);
+            let red = parseInt(redHex, 16);
+            let green = parseInt(greenHex, 16);
+            let blue = parseInt(blueHex, 16);
 
-    let color = `#${red16}${green16}${blue16}`;
-    console.log(color);
-    let body = document.getElementById('body');
-    body.style.backgroundColor = color;
+            if (red * 0.299 + green * 0.587 + blue * 0.114 > 186) {
+                body.style.color = '#000000';
+            } else {
+                body.style.color = '#ffffff';
+            }
 
-    if (red * 0.299 + green * 0.587 + blue * 0.114 > 186) {
-        body.style.color = '#000000';
+            let hentaiCode =
+                '' + Math.round((red / 255) * 99) + Math.round((green / 255) * 99) + Math.round((blue / 255) * 99);
+
+            colorOut.innerText = hentaiCode;
+        }
+    } else if (isNaN(value) || value < 0 || value > 999999) {
+        return alert('enter hentai code from 000000 - 999999, or hexadecimal color code from #000000 - #ffffff');
     } else {
-        body.style.color = '#ffffff';
+        value = value.toString();
+        while (value.length < 6) {
+            value = '0' + value;
+        }
+        console.log(value);
+        let r = parseInt(value.substring(0, 2));
+        let g = parseInt(value.substring(2, 4));
+        let b = parseInt(value.substring(4, 6));
+
+        let red = Math.round((r / 99) * 255);
+        let green = Math.round((g / 99) * 255);
+        let blue = Math.round((b / 99) * 255);
+
+        red16 = red.toString(16) + '';
+        green16 = green.toString(16) + '';
+        blue16 = blue.toString(16) + '';
+        if (red16.length == 1) red16 = '0' + red16;
+        if (green16.length == 1) green16 = '0' + green16;
+        if (blue16.length == 1) blue16 = '0' + blue16;
+
+        let color = `#${red16}${green16}${blue16}`;
+        console.log(color);
+        let body = document.getElementById('body');
+
+        body.style.backgroundColor = color;
+        if (red * 0.299 + green * 0.587 + blue * 0.114 > 186) {
+            body.style.color = '#000000';
+        } else {
+            body.style.color = '#ffffff';
+        }
+        colorOut.innerText = color;
     }
-    colorOut.innerText = color;
 }
 hentai.value = '200834';
